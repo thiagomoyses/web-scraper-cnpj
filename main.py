85f481bd934114caffd66ed5d2f0102386ff4d30
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-09-30 19:29:08 +0200

    Fix looping of __filter_informations

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from modules.webscraper import WebScrapper
 
 if __name__ == "__main__":
     
+    counter = 1
+    result = []
+    
     while True:
         uf = input("-> UF: ").upper()
         city = input("-> City: ").upper()
+        page_number = int(input("-> Page numeber: "))
         
-        if uf and city :
+        if uf and city and page_number and page_number > 0:
             break
         
-        print("You need to inform UF and city!")
+        print("You need to inform UF, city and how many pages you wish to scrap!")
     
     web_scrapper = WebScrapper(state=uf, city=city)
-    result = web_scrapper.get_cnpj_and_infos()
     
-    print(result)
+    while counter <= page_number:
+        result.append(web_scrapper.get_cnpj_and_infos(counter))
+        print(result)
+        
+        counter += 1
--- modules/webscraper.py
+++ modules/webscraper.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import os
-import re
+import time
 
 from bs4 import BeautifulSoup
+
 
 class WebScrapper:
     def __init__(self, state, city):
@@ -12,79 +13,74 @@ class WebScrapper:
             "Host": "api.casadosdados.com.br",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
             "content-type": "application/json",
-            "Accept": "/"
+            "Accept": "/",
+            "Content-Length": "500",
         }
         self.state = state
         self.city = city
-        
-    def get_cnpj_and_infos(self):
+
+    def get_cnpj_and_infos(self, page):
         parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         json_path = os.path.join(parent_dir, "json", "request_body.json")
-        
+
         with open(json_path, "r") as file:
             request_body = json.load(file)
-            
-        request_body['query']['uf'] = self.state
-        request_body['query']['municipio'] = self.city
-            
-        request = requests.post(self.base_url, headers=self.headers)
-        
+
+        request_body["query"]["uf"] = [self.state]
+        request_body["query"]["municipio"] = [self.city]
+        request_body["page"] = page
+
+        body = json.dumps(request_body)
+
+        request = requests.post(self.base_url, data=body, headers=self.headers)
+
+        if request.status_code != 200:
+            return []
+
         data = json.loads(request.text)
-        cnpj_list = data.get('data', {}).get('cnpj', [])
-        
-        get_html_info = self.__get_companies_information(cnpj_list)
-        
+        cnpj_list = data.get("data", {}).get("cnpj", [])
+
+        get_html_info = self.__get_companies_information(cnpj_list, body)
+
         filtered_informations = self.__filter_informations(get_html_info)
-        
+
         return filtered_informations
-    
-    def __get_companies_information(self, cnpj_list):
-        
+
+    def __get_companies_information(self, cnpj_list, body):
         base_url = "https://casadosdados.com.br/solucao/cnpj/"
         url_list = []
         companies_html = []
-        self.headers.pop("content-type", None)
-        self.headers['Host'] = "casadosdados.com.br"
-        
+        self.headers["Host"] = "casadosdados.com.br"
+
         for cnpj_info in cnpj_list:
-            
-            cnpj = cnpj_info['cnpj']
-            razao_social = cnpj_info['razao_social'].replace(" ", "-")
+            cnpj = cnpj_info["cnpj"]
+            razao_social = cnpj_info["razao_social"].replace(" ", "-")
             url = f"{base_url}{razao_social}-{cnpj}"
             url_list.append(url)
-        
+
         for company_url in url_list:
-            
-            request = requests.get(company_url, headers=self.headers)
+            request = requests.get(company_url, data=body, headers=self.headers)
             companies_html.append(request)
-            
+
         return companies_html
-    
+
     def __filter_informations(self, html_array):
-        
-        white_list = {
-            "CNPJ": "",
-            "Razão Social": "",
-            "E-MAIL": "",
-            "Telefone": ""
-        }
-        
         companies_informations = []
-        
+
         for elements in html_array:
-                soup = BeautifulSoup(elements.text, 'html.parser')
-                info_element = soup.find_all('div', class_='column is-narrow')
-                
-                for data in info_element:
-                    data_text = data.text
-                    
-                    for key in white_list:
-                        if key in data_text:
-                            filtered_text = data_text.replace(key, "")
-                            white_list[key] = filtered_text
-                            companies_informations.append(white_list)
-        
+            soup = BeautifulSoup(elements.text, "html.parser")
+            info_element = soup.find_all("div", class_="column is-narrow")
+
+            white_list = {"CNPJ": "", "Razão Social": "", "E-MAIL": "", "Telefone": ""}
+
+            for data in info_element:
+                data_text = data.text
+
+                for key in white_list:
+                    if key in data_text:
+                        filtered_text = data_text.replace(key, "")
+                        white_list[key] = filtered_text
+
+            companies_informations.append(white_list)
+
         return companies_informations
-                    
-                
-            
